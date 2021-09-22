184a8ece5c0842011cfd27a9f2dd705789b88bee
gajadhav-ccrn <gjadhav@crosscountry.com>
2021-09-22 12:58:32 +0530

    Initial commit

---
+++ models/students.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 33,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  enrollnumber: {
+    type: Number,
+    min: 1,
+    max: 120
+  }
+});
+
+const Student = mongoose.model('STUDENT', studentSchema);
+module.exports = Student;
+
